f308a461011b0c54fdef3f5ebd3a7463ef199b44
abburinagasaiajaykumar116 <109759425+abburinagasaiajaykumar116@users.noreply.github.com>
2023-05-17 16:14:01 +0530

    Add files via upload

--- backend/middleware/protectedSource.js
+++ backend/middleware/protectedSource.js
@@ -1,1 +1,26 @@
+const jwt=require('jsonwebtoken');
+const {SECRET_TOKEN}=require('../config');
+const mongoose=require('mongoose');
+const UserModel=mongoose.model('UserModel');
+module.exports=(req,res,next)=>{
+  const {authorization} = req.headers;
+  //authorization-> Bearer abgsjjbaebjdbjd
+  if(!authorization){
+    return   res.status(400).json({error:"user not logged in"});
+  }
+  const token = authorization.toString().replace("Bearer ","");
+  jwt.verify(token,SECRET_TOKEN,(error,payload)=>{
+   if(error){
+      return res.status(400).json({error:"user  not logged in"});
+   }
+   const {_id} = payload;
+   UserModel.findById(_id)
+   .then(dbUser=>{
+     req.dbUser=dbUser;
+     next();
+   });
 
+
+  });
+
+}
